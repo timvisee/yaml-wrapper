880d2952f39035d7d59d1bbda98f1d95763796b6
timvisee <timvisee@gmail.com>
2017-02-14 01:33:14 +0100

    Clean FileConfiguration class

--- src/main/java/com/timvisee/yamlwrapper/FileConfiguration.java
+++ src/main/java/com/timvisee/yamlwrapper/FileConfiguration.java
@@ -12,103 +12,113 @@ import java.io.InputStreamReader;
 public abstract class FileConfiguration extends Configuration {
     
 	/**
-	 * Constructor
+	 * Constructor.
 	 */
     public FileConfiguration() {
         super();
     }
 
     /**
-     * Save the configuration to a file
-     * @param file File to save the configuration in
-     * @throws IOException
+     * Save the configuration to the given file.
+     *
+     * @param file File to save the configuration in.
+     *
+     * @throws IOException Throws if failed to save the configuration to the given file.
      */
     public void save(File file) throws IOException {
-        // Make sure the file is not null
+        // The file may not be null
     	if(file == null)
     		return;
 
-        // Create the parent directories
+        // Create the parent directories if they don't exist
     	file.getParentFile().mkdirs();
     	
     	// Get the configuration string
         String data = saveToString();
 
         // Construct the file writer
-        FileWriter writer = new FileWriter(file);
 
         // Save the data
-        try {
+        try(FileWriter writer = new FileWriter(file)) {
             writer.write(data);
-        } finally {
-            writer.close();
         }
     }
     
     /**
-     * Save the configuration to a file
-     * @param file File path to save the configuration at
-     * @throws IOException
+     * Save the configuration to the given file.
+     *
+     * @param filePath Path of the file to save the configuration in.
+     *
+     * @throws IOException Throws if failed to save the configuration to the given file.
      */
-    public void save(String file) throws IOException {
-        // Make sure the file path is not null
-    	if(file == null || file.equals(""))
+    public void save(String filePath) throws IOException {
+        // Make sure the file path is not null or empty
+    	if(filePath == null || filePath.equals(""))
     		return;
     	
     	// Save the file
-        save(new File(file));
+        save(new File(filePath));
     }
 
     /**
-     * Save the configuration to a string
-     * @return Configuration string
+     * Save the configuration to a string.
+     *
+     * @return Configuration string.
      */
     public abstract String saveToString();
 
     /**
-     * Load the configuration from a file
-     * @param file File to load the configuration from
-     * @throws FileNotFoundException
-     * @throws IOException
+     * Load a configuration file from the given file.
+     *
+     * @param file File to load the configuration from.
+     *
+     * @throws FileNotFoundException Throws if the file that was given, doesn't exist.
+     * @throws IOException Throws if failed to load the configuration file.
      */
     public void load(File file) throws FileNotFoundException, IOException {
-        // Load the file
         load(new FileInputStream(file));
     }
 
     /**
-     * Load the configuration from a input stream
-     * @param stream Input stream to load the configuration from
-     * @throws IOException
+     * Load the configuration from an input stream.
+     *
+     * @param stream Input stream to load the configuration from.
+     *
+     * @throws IOException Throws if failed to load the configuration from the given input stream.
      */
     public void load(InputStream stream) throws IOException {
         // Make sure the input stream is not null
     	if(stream == null)
     		return;
 
+    	// Create the proper readers to read the file
         InputStreamReader reader = new InputStreamReader(stream);
         StringBuilder builder = new StringBuilder();
-        BufferedReader input = new BufferedReader(reader);
 
-        try {
+        // Safely read the file from the input reader
+        try (BufferedReader input = new BufferedReader(reader)) {
+            // Variable to hold the current line
             String line;
 
+            // Read the file line by line
             while ((line = input.readLine()) != null) {
                 builder.append(line);
                 builder.append('\n');
             }
-        } finally {
-            input.close();
+
         }
-        
+
+        // Load the file from the given string
         loadFromString(builder.toString());
     }
 
     /**
-     * Load the configuration from a file path
-     * @param file File path to the configuration file
-     * @throws FileNotFoundException
-     * @throws IOException
+     * Load a configuration from the given file path.
+     *
+     * @param file Path of the file holding the configuration to load.
+     *
+     * @throws FileNotFoundException Thrown when the given file path doesn't exist.
+     * @throws IOException Thrown if failed to load the configuration.
      */
     public void load(String file) throws FileNotFoundException, IOException {
         // Make sure the file path is not null
@@ -120,8 +130,9 @@ public abstract class FileConfiguration extends Configuration {
     }
 
     /**
-     * Load the configuration from a string
-     * @param contents Configuration string
+     * Load the configuration from the given string.
+     *
+     * @param contents Configuration string.
      */
     public abstract void loadFromString(String contents);
 }
